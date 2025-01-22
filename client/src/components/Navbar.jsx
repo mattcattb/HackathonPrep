76f344a5be0f16df7026833c91c7ae5d2f0fdc79
Matthew <mboughton17@gmail.com>
2025-01-22 15:08:57 -0500

    added nav and router!

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-[40px] bg-slate-800'>
+      <div className='flex flex-col justify-normal'>
+        <Link to="/">Home</Link>
+        <Link to="/one">Page one</Link>
+        <Link to="/two">Page two</Link>
+      </div>
+    </div>
   )
 }
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+
 import LandingPage from './pages/LandingPage';
 import OnePage from './pages/OnePage';
 import TwoPage from './pages/TwoPage';
